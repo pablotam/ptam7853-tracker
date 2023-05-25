fa7da7ac67c5e556173a30d56f168185fb6dfc7a
pabzluis <121782500+pabzluis@users.noreply.github.com>
2023-05-25 12:43:05 +1000

    added buttons and second page

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const tasteButton = document.getElementById('create-taste')
+const createTaste = document.getElementById('create-your-taste')
+
+tasteButton.addEventListener('click', () => {
+    createTaste.style.display = 'block';
+});
+// create taste button 
+
+
