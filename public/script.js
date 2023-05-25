ede7fe5c2cd6c4bd8f19c6dccb11f4ef60e74232
pabzluis <121782500+pabzluis@users.noreply.github.com>
2023-05-25 13:16:02 +1000

    added flexbox and translation

--- public/script.js
+++ public/script.js
@@ -1,9 +1,19 @@
 const tasteButton = document.getElementById('create-taste')
 const createTaste = document.getElementById('create-your-taste')
+const homePage = document.getElementById('home-page')
 
 tasteButton.addEventListener('click', () => {
     createTaste.style.display = 'block';
+    homePage.style.display = 'none'; 
 });
 // create taste button 
 
 
+const backButton = document.getElementById('buttons-sp')
+
+
+backButton.addEventListener('click', () => {
+    createTaste.style.display = 'none';
+    homePage.style.display = 'block'; 
+});
+// second page 
